895bf037a805a1f4cf5091c17887b863699b783a
parkgiwon <parkgo0504>
2022-10-25 14:13:36 +0900

    push

--- app/src/main/java/com/example/smartlocker10/Activity.java
+++ app/src/main/java/com/example/smartlocker10/Activity.java
@@ -2,38 +2,86 @@ package com.example.smartlocker10;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 
 
 public class Activity extends AppCompatActivity {
-    ImageView load;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activtiy_load);
-        load=(ImageView)findViewById(R.id.loadimg);
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageReference = storage.getReference();
-        StorageReference pathReference = storageReference.child("1.jpg");
-        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Glide.with(Activity.this).load(uri).into(load);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
+        StorageReference listRef = storage.getReference().child("ImageFolder");
 
-            }
-        });
+        listRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    int i = 1;
+
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the prefixes under listRef.
+                            // You may call listAll() recursively on them.
+
+                            // imageview와 textview를 생성할 레이아웃 id 받아오기
+                            LinearLayout layout = (LinearLayout) findViewById(R.id.show);
+                            // textview 동적생성
+                            TextView tv = new TextView(Activity.this);
+                            tv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                            tv.setText(Integer.toString(i) + ". new TextView");
+                            tv.setTextSize(30);
+                            tv.setTextColor(0xff004497);
+                            layout.addView(tv);
+
+                            //imageview 동적생성
+                            ImageView iv = new ImageView(Activity.this);
+                            iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                            layout.addView(iv);
+                            i++;
+
+
+                            // reference의 item(이미지) url 받아오기
+                            item.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()) {
+                                        // Glide 이용하여 이미지뷰에 로딩
+                                        Glide.with(Activity.this)
+                                                .load(task.getResult())
+                                                .into(iv);
+                                    } else {
+                                        // URL을 가져오지 못하면 토스트 메세지
+                                        Toast.makeText(Activity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Uh-oh, an error occurred!
+                                }
+                            });
+                        }
+                    }
+                });
+
     }
 }
+
+
