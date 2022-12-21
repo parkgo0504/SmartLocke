93e9992128a5490dac241b5e0faf94853eb69355
chosun <prakgo0504@naver.com>
2022-12-21 20:29:21 +0900

    Realtime db

--- app/src/main/java/com/example/smartlocker10/MainActivity2.java
+++ app/src/main/java/com/example/smartlocker10/MainActivity2.java
@@ -31,41 +31,12 @@ import java.util.HashMap;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
-    private static final int PICK_IMAGE = 1;
-
-    Button upload, choose;
-    TextView alert;
-    ArrayList<Uri> ImageList = new ArrayList<>();
-    private Uri ImageUri;
-    private ProgressDialog progressDialog;
-    private int upload_count = 0;
-
-    private Button button;
-
-    private ImageView imageView;
-    private ProgressBar progressBar;
-    private final DatabaseReference root = FirebaseDatabase.getInstance().getReference("Image");
-    private final StorageReference reference = FirebaseStorage.getInstance().getReference();
-
-    private Uri imageUri;
-
-
-
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference conditionRef = mRootRef.child("text");
-
-
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main2);   //메인 2레이아웃 실행
 
         EditText editText = (EditText) findViewById(R.id.Date);
-
-
-
-
 
         //파이어베이스에서 이미지 불러오기
         Button Button_load = (Button) findViewById(R.id.image_load);
@@ -86,83 +57,7 @@ public class MainActivity2 extends AppCompatActivity {
                 }
             }
         });
-
-
-
-
-
-
-
-
-    } // onCreate
-
-
-    private void StoreLink(String url){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("UserOne");
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("Imagelink",url);
-
-        databaseReference.push().setValue(hashMap);
-
-        progressDialog.dismiss();
-        alert.setText("Image Uploaded Successfully");
-        upload.setVisibility(View.GONE);
-
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == PICK_IMAGE){
-            if(resultCode == RESULT_OK){
-                if(data.getClipData() != null){
-
-                    int countClipData = data.getClipData().getItemCount();
-
-                    int currentImageSelect = 0;
-                    while(currentImageSelect < countClipData){
-
-                        ImageUri = data.getClipData().getItemAt(currentImageSelect).getUri();
-                        ImageList.add(ImageUri);
-                        currentImageSelect = currentImageSelect +1;
-                    }
-
-                    alert.setVisibility(View.VISIBLE);
-                    alert.setText("YOU HAVE SELECTED"+ImageList.size()+"Images");
-                    choose.setVisibility(View.GONE);
-                }
-                else{
-                    Toast.makeText(this, "Please choose images", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    // 사진 가져오기
-    ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        imageUri = result.getData().getData();
-
-                        imageView.setImageURI(imageUri);
-                    }
-                }
-            });
-
-
-
-    //파일타입 가져오기
-    private String getFileExtension(Uri uri){
-
-        ContentResolver cr = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-
-        return mime.getExtensionFromMimeType(cr.getType(uri));
-    }
-
 
 
 
